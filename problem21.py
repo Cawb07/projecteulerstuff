584b7bd250ffc3b9516704ddcda77b0933a0e467
Cawb07 <nbg235@nyu.edu>
2013-08-27 23:59:54 -0400

    Finds Amicable numbers (avoids Perfect numbers) and sums them.

--- problem21.py
+++ problem21.py
@@ -3,12 +3,57 @@ Created on Aug 18, 2013
 
 https://projecteuler.net/problem=21
 
-Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
-If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
-
 For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
 
 Evaluate the sum of all the amicable numbers under 10000.
 
 @author: Cawb07
 '''
+import time
+
+def proper_divisors(n):
+    divisors = []
+    i = 1
+    
+    if (n/2)%1 == 0:
+        while i < (n/2)+1:
+            if n%i == 0:
+                divisors.append(i)
+            i += 1
+    else:
+        while i < (n+1)/2:
+            if n%i == 0:
+                divisors.append(i)
+            i += 1
+    
+    return divisors
+
+
+def sum(d):
+    sigma = 0
+    
+    for e in d:
+        sigma += e
+    
+    return sigma
+
+
+def amicables_under(n):
+    amicables = []
+    
+    for i in range(1, n):
+        pair = sum(proper_divisors(i))
+        
+        if pair != i:    
+            if sum(proper_divisors(pair)) == i:
+                amicables.append(i)
+    
+    return amicables
+
+
+start = time.time()
+
+print sum(amicables_under(10000))
+
+elapsed = time.time() - start
+print "The elapsed time is %s seconds." % (elapsed)
