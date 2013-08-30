10b23963fa8a2563da0aa8045838e880d493de79
Cawb07 <nbg235@nyu.edu>
2013-08-30 01:01:28 -0400

    Halfway done.

---
+++ problem23.py
@@ -0,0 +1,89 @@
+'''
+Created on Aug 29, 2013
+
+http://projecteuler.net/problem=23
+
+A perfect number is a number for which the sum of its proper divisors is exactly
+equal to the number. For example, the sum of the proper divisors of 28 would be
+1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+
+A number n is called deficient if the sum of its proper divisors is less than n
+and it is called abundant if this sum exceeds n.
+
+As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest
+number that can be written as the sum of two abundant numbers is 24.
+By mathematical analysis, it can be shown that all integers greater than 28123
+can be written as the sum of two abundant numbers. However, this upper limit
+cannot be reduced any further by analysis even though it is known that the
+greatest number that cannot be expressed as the sum of two abundant numbers is
+less than this limit.
+
+Find the sum of all the positive integers which cannot be written as the sum of
+two abundant numbers.
+
+@author: Cawb07
+'''
+import time
+
+def proper_divisors(n):
+    divisors = []
+    i = 1
+    
+    if (n/2)%1 == 0:
+        while i < (n/2)+1:
+            if n%i == 0:
+                divisors.append(i)
+            i += 1
+    else:
+        while i < (n+1)/2:
+            if n%i == 0:
+                divisors.append(i)
+            i += 1
+    
+    return divisors
+
+
+def sum(d):
+    sigma = 0
+    
+    for e in d:
+        sigma += e
+    
+    return sigma
+
+def is_abundant(n):
+    
+    if sum(proper_divisors(n)) > n:
+        return True
+    else:
+        return False
+    
+    
+def abundants_under(n):
+    abundants = []
+    
+    for i in range(1, n):
+        if is_abundant(i):    
+            abundants.append(i)
+    
+    return abundants
+
+
+def sum_all_pairs(n):
+    sums = []
+    
+    for i in range(0, len(abundants)):
+        for j in range(i, len(abundants)):
+            sums.append( abundants[i]+abundants[j] )
+    
+    return sums
+
+
+
+start = time.time()
+
+abundants = abundants_under(16000)
+print sum_all_pairs(abundants)
+
+elapsed = time.time() - start
+print "The elapsed time is %s seconds." % (elapsed)
